ddd0808f03e6317f17adb44123e78ed970095ce2
Riyard20 <kamarudeenriyad2@gmail.com>
2024-11-11 22:06:24 -0600

    exercise 1 done

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace SelectionStatementExercise
+﻿namespace SelectionStatementExercise;
+
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        var random = new Random();
+        var favnumbers = random.Next(1, 11);
+        int response = 0;
+
+        while (response != favnumbers)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            Console.WriteLine("Give me a number between 1 and 10:");
+            response = int.Parse(Console.ReadLine());
+
+            if (response < favnumbers)
+            {
+                Console.WriteLine($"Too low!\nYour guess: {response}");
+            }
+            else if (response > favnumbers)
+            {
+                Console.WriteLine($"Too high!\nYour guess: {response}");
+            }
+            else
+            {
+                Console.WriteLine($"Your guessed it!\nThe favorite number was  {favnumbers}!");
+            }
         }
     }
 }
